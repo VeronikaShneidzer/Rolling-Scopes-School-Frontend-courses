0ecc4e4413e073b49bdffc295ceb0c2556a5d733
Veranika Shneidzer <veranika.shneidzer@thomsonreuters.com>
2019-07-02 15:53:05 +0300

    add Smart calculator task

---
+++ Smart calculator/src/index.js
@@ -0,0 +1,36 @@
+class SmartCalculator {
+  constructor(initialValue) {
+    this.str = '' + initialValue;
+  }
+
+  add(number) {
+    this.str += '+' + number;
+    return this;
+  }
+
+  subtract(number) {
+    this.str += '-' + number;
+    return this;
+  }
+
+  multiply(number) {
+    this.str += '*' + number;
+    return this;
+  }
+
+  devide(number) {
+    this.str += '/' + number;
+    return this;
+  }
+
+  pow(number) {
+    this.str += '**' + number;
+    return this;
+  }
+
+  valueOf() {
+    return eval(this.str);
+  }
+}
+
+module.exports = SmartCalculator;
