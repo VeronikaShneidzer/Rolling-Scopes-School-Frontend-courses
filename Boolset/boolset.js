db3dea13fb8174108e1ae25ae4c9712d7692a517
NikaNeider <nika.neider@gmail.com>
2020-05-20 00:21:52 +0300

    Clean code, add 04 part for JS Assignments

--- Boolset/boolset.js
+++ Boolset/boolset.js
@@ -8,26 +8,21 @@ function boolset() {
 				];
 
 	let arr = _.chunk(array,2);
-	console.log(arr);
 	// => [['a', true],['b', true],['c', false],['d', true],['e', false]];
 
 	arr = _.fromPairs(arr);
-	console.log(arr);
 	// => {a: true, b: true, c: false, d: true, e: false}
 
 	let a = _.reduce(arr, function(result, value, key) {
 		(result[value] || (result[value] = [])).push(key);
   		return result;
 		}, {});
-	console.log(a);
 	// => {true: ['a', 'b', 'd'], false: ['c', 'e']}
 
 	let c = _.map(a, (x, y) => {  return x});
-	console.log(c);
 	// => [{true: ['a', 'b', 'd']}, {false: ['c', 'e']}]
 
 	let b = _.head(c);
-	console.log(b);
 	// => ['a', 'b', 'd']
 
 	// In one string^
